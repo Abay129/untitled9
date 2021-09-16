9616be12ef647f81ae056d2597f8bf3d362cadf4
Abay129 <satybekov.129@bk.ru>
2021-09-16 14:41:00 +0300

    ADD new clases start 2 task

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -25,7 +25,20 @@ public class Main {
 
         //Printer.print(cats);
 
+        // исправьте код так, что бы на экран выводилось что-то вроде.
+        // Я Misty. Я прыгаю!
+        // Я Tibbles. Я сплю!
+        // Я Ginger. Я кушаю!
+        // для решения примените лямбда-выражения,
 
+        // каждый кот должен уметь выполнять что-то своё
+        var cats = new ArrayList<ActiveCat>();
+        cats.add(new ActiveCat());
+        cats.add(new ActiveCat(???));
+        cats.add(new ActiveCat(???));
+        // добавьте ещё два-три кота, с совершенно другими действиями
+
+        cats.forEach(ActiveCat::doAction);
     }
 
 }
